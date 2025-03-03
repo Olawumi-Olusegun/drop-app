ff1920ed3e56bed4520add9cb571a459379d05bd
Oluwafemi <femio82@gmail.com>
2025-03-03 21:00:50 +0100

    feat:registration endpoint for users

--- src/services/driver.service.ts
+++ src/services/driver.service.ts
@@ -1,5 +1,8 @@
 
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, RegistrationStatus, verificationType } from '@prisma/client';
+import { DocumentUploadPayload, DriverRegistrationInput } from "../types";
+import { generatePresignedUrl } from "../utils/s3";
+import {Response} from 'express';
 
 const prisma = new PrismaClient();
 
@@ -83,3 +86,131 @@ const haversineDistance = (
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   return R * c;
 };
+
+export const registerDriver = async (data: DriverRegistrationInput)=>{
+
+  const dateOfBirthISO = data.dateOfBirth.includes('T')
+  ? data.dateOfBirth
+  : data.dateOfBirth + 'T00:00:00Z';
+  const userExists = await prisma.user.findUnique({ where: { id: data.userId } });
+  if (!userExists) {
+    throw new Error(`User with id ${data.userId} does not exist.`);
+  }
+
+  
+  const existingDriver = await prisma.driver.findUnique({ where: { userId: data.userId } });
+  if (existingDriver) {
+    throw new Error(`Driver with id ${existingDriver.id} already exists.`);
+  }
+  const driver = await prisma.driver.create({
+    data:{
+      userId: data.userId,
+      firstName: data.firstName,
+      middleName: data.middleName,
+      lastName: data.lastName,
+      nationality: data.nationality,
+      dateOfBirth: new Date(dateOfBirthISO),
+      fullAddress: data.address,
+      city: data.city,
+      postalCode: data.postalCode,
+      country: data.country,
+      totalCompletedRides: 0,
+      registrationStatus: RegistrationStatus.pending,
+      registrationDate: new Date()
+    }
+
+  })
+  await prisma.driverIdentification.create({
+    data:{
+      driverId: driver.id,
+      issuingCountry: data.issuingCountry,
+      documentType: data.verificationType,
+      nin: data.nin,
+      licenseNumber: data.licenseNumber,
+      licenseExpiryDate: data.licenseExpiryDate,
+      passportPhotoUrl: '',
+      idCardBackUrl: '',
+      idCardFrontUrl: '',
+      licensePhotoUrl: '',
+      selfieWithLicenseUrl: ''
+    }
+  })
+
+  await prisma.driverVehicle.create({
+    data:{
+      driverId: driver.id,
+      carBrand: data.carBrand,
+      carModel: data.carModel,
+      licensePlateNumber: data.licensePlateNumber,
+      carColor: data.carColour,
+      carPictureUrl: '',
+      vehicleRegistration: '',
+      roadWorthiness: ''
+
+    }
+  })
+
+  const passportPhotoKey = `drivers/${driver.id}/passportPhoto.jpg`;
+  const idCardFrontKey =`drivers/${driver.id}/licensePhoto.jpg`;
+  const idCardBackKey = `drivers/${driver.id}/idCardBack.jpg`
+  
+  const licensePhotoKey = `drivers/${driver.id}/licensePhoto.jpg`;
+  const selfieWithLicenseKey = `drivers/${driver.id}/selfieWithLicense.jpg`;
+  const carPictureKey = `drivers/${driver.id}/carPicture.jpg`;
+  const vehicleRegistration = `drivers/${driver.id}/vehicleRegistration.jpg`
+  const roadWorthiness = `drivers/${driver.id}/roadWorthiness.jpg`
+  
+  
+/*
+  const [passPortPhotoUrl, idCardFrontUrl, idCardBackUrl, licensePhotoUrl, selfieWithLicenseUrl, carPictureUrl, vehicleRegistrationUrl, roadWorthinessUrl] = await Promise.all([
+    generatePresignedUrl(passportPhotoKey),
+    generatePresignedUrl(idCardFrontKey),
+    generatePresignedUrl(idCardBackKey),
+    generatePresignedUrl(licensePhotoKey),
+    generatePresignedUrl(selfieWithLicenseKey),
+    generatePresignedUrl(carPictureKey),
+    generatePresignedUrl(vehicleRegistration),
+    generatePresignedUrl(roadWorthiness)
+  ])
+  const preSignedUrls = {
+    passPortPhotoUrl, idCardFrontUrl, idCardBackUrl,
+     licensePhotoUrl, 
+    selfieWithLicenseUrl, carPictureUrl, vehicleRegistrationUrl,
+     roadWorthinessUrl
+  }
+     */
+
+  return {driver} //preSignedUrls}
+}
+
+export const updateDriverDocuments = async(payload: DocumentUploadPayload)=>{
+  const {driverId,documents}= payload;
+
+  const driver = await prisma.driver.findUnique({where:{id: driverId}})
+  if(!driver){
+    throw new Error("Driver not Found") 
+  }
+
+  await prisma.driverIdentification.update({
+    where: {driverId},
+   data:{
+      passportPhotoUrl: documents.passportPhotoUrl ? documents.passportPhotoUrl: undefined,
+      idCardFrontUrl: documents.idCardFrontUrl ? documents.idCardFrontUrl : undefined,
+      idCardBackUrl: documents.idCardBackUrl? documents.idCardBackUrl : undefined,
+      licensePhotoUrl: documents.licensePhotoUrl,
+      selfieWithLicenseUrl: documents.selfieWithLicenseUrl,
+   }
+
+   
+  })
+
+  await prisma.driverVehicle.update({
+    where:{driverId},
+    data:{
+      carPictureUrl: documents.carPictureUrl,
+      roadWorthiness: documents.roadWorthiness
+    }
+  })
+  return { message: 'Documents updated successfully' };
+
+}
