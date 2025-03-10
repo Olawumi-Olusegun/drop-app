d54349cd1232d10bd804129e80986497699adbf1
Olawumi Olusegun <olawumi.olusegun@gmail.com>
2025-03-10 20:12:50 +0100

    Updated ride and driver schema with controller

--- src/services/driver.service.ts
+++ src/services/driver.service.ts
@@ -415,7 +415,7 @@ export const cancelRideBid = async (
   const updatedBid = await prisma.rideBid.update({
     where: { id: bid.id },
     data: {
-      status: BidStatus.cancelled,
+      status: BidStatus.rejected,
     },
   });
 
