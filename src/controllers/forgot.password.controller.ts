ef7f90acc74221b56c9c7f45f986fb3ce21f7118
Olawumi Olusegun <olawumi.olusegun@gmail.com>
2025-03-12 13:46:33 +0100

    Update: Fixed bug in reset-password controller

--- src/controllers/forgot.password.controller.ts
+++ src/controllers/forgot.password.controller.ts
@@ -9,16 +9,16 @@ import { expirationTime } from "../utils/timeExpiry";
 
 
 export const forgotPassword = async (req: Request, res: Response) => {
-  
+
   const { email, phoneNumber } = req.body;
 
   try {
 
     let user = null;
-
-    const formattedPhoneNumber = formatPhoneNumber(phoneNumber);
-
-    if (formattedPhoneNumber) {
+    let formattedPhoneNumber = null;
+    
+    if (phoneNumber) {
+      formattedPhoneNumber = formatPhoneNumber(phoneNumber);
       user = await prisma.user.findFirst({ where: { phoneNumber: formattedPhoneNumber } });
     } else if (email) {
       user = await prisma.user.findUnique({ where: { email } });
@@ -50,6 +50,7 @@ export const forgotPassword = async (req: Request, res: Response) => {
 
     return res.status(Statuscode.SUCCESS).json({ message: "OTP sent successfully. Check your email or phone." });
   } catch (error) {
+    console.log(error)
     return res.status(Statuscode.INTERNAL_SERVER_ERROR).json({ message: "An error occurred. Please try again later." });
   }
 };
