96d6dca67393d693c951f487e171f63925123280
Oluwafemi <femio82@gmail.com>
2025-03-13 18:15:38 +0100

    fixed issues

--- src/routes/admin.route.ts
+++ src/routes/admin.route.ts
@@ -1,17 +1,19 @@
 import { Router } from "express";
 import { approveDriver, getAdminDashboardStats, getAllDrivers, getAllPendingDrivers, getAllUsers, getDriver, getUser, suspendDriver } from "../controllers/admin.controllr";
 import { authenticateUser, authorizeRole } from "../middlewares/auth.middleware";
-import { UserRole } from "@prisma/client";
+
 import { validateapproveDriver, validateGetAllDrivers, validateGetAllUsers, validateGetDriver, validateGetUSer, validatesuspendDriver } from "../validators/adminValidations";
+import { UserRole } from "../types";
 
 const router = Router()
 
-router.get('/get-all-users', authenticateUser,authorizeRole([UserRole.admin]),validateGetAllUsers,getAllUsers)
-router.get('/get-user',authenticateUser,authorizeRole([UserRole.admin]) , validateGetUSer, getUser)
-router.get('/get-all-drivers',authenticateUser, authorizeRole([UserRole.admin]), validateGetAllDrivers ,getAllDrivers)
-router.get('/getdriver',authenticateUser, authorizeRole([UserRole.admin]), validateGetDriver, getDriver)
-router.post('/approve-driver',authenticateUser, authorizeRole([UserRole.admin]), validateapproveDriver , approveDriver)
-router.get('/pending-drivers',authenticateUser, authorizeRole([UserRole.admin]) , getAllPendingDrivers)
-router.get('/admin-dashboard',authenticateUser, authorizeRole([UserRole.admin]),getAdminDashboardStats)
-router.get('/suspend-driver',authenticateUser, authorizeRole([UserRole.admin]), validatesuspendDriver, suspendDriver)
+router.get('/get-all-users', authenticateUser,authorizeRole([UserRole.ADMIN]),validateGetAllUsers,getAllUsers)
+router.get('/get-user',authenticateUser,authorizeRole([UserRole.ADMIN]) , validateGetUSer, getUser)
+router.get('/get-all-drivers',authenticateUser, authorizeRole([UserRole.ADMIN]), validateGetAllDrivers ,getAllDrivers)
+router.get('/get-driver',authenticateUser, authorizeRole([UserRole.ADMIN]), validateGetDriver, getDriver)
+router.post('/approve-driver',authenticateUser, authorizeRole([UserRole.ADMIN]), validateapproveDriver , approveDriver)
+router.post('/suspend-driver',authenticateUser, authorizeRole([UserRole.ADMIN]), validatesuspendDriver, suspendDriver)
+router.get('/pending-drivers',authenticateUser, authorizeRole([UserRole.ADMIN]) , getAllPendingDrivers)
+router.get('/admin-dashboard',authenticateUser, authorizeRole([UserRole.ADMIN]),getAdminDashboardStats)
+
 export default router
