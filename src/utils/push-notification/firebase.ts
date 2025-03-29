86145d8c15eb0721027ed437e3fcc63fe112feb4
Olawumi Olusegun <olawumi.olusegun@gmail.com>
2025-03-29 14:21:26 +0100

    Updated firebase admin

--- src/utils/push-notification/firebase.ts
+++ src/utils/push-notification/firebase.ts
@@ -1,14 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
 import admin from 'firebase-admin';
-const serviceAccount = require('./path-to-serviceAccountKey.json');
 
-admin.initializeApp({ credential: admin.credential.cert(serviceAccount),});
+// const serviceAccount = require('./path-to-serviceAccountKey.json');
+// admin.initializeApp({ credential: admin.credential.cert(serviceAccount),});
 
-// admin.initializeApp({
-//     credential: admin.credential.cert({
-//       projectId: process.env.FIREBASE_PROJECT_ID,
-//       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-//       privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-//     }),
-// });
+admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID!,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+    }),
+});
 
 export default admin;
